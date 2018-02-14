3bb9cef682df269c8d76ff90df18640f647a5f74
Jimmy Kumpulainen <jimmy@kumpulainen.se>
2018-02-14 20:51:20 +0100

    Fix for typo, version bump

--- blueberry.lua
+++ blueberry.lua
@@ -3,13 +3,14 @@
 local bluebuff = "Entropic Embrace"
 local buffIsActive = false;
 local frame = CreateFrame("FRAME", "BlueberryFrame")
+local willPlay, soundHandle = nil;
 
 frame:RegisterUnitEvent("UNIT_AURA")
 frame:SetScript("OnEvent", function(self, event, ...)
   if UnitBuff("player", bluebuff) and not buffIsActive then
-    buffIsActive = true;
-    PlaySoundFile("Interface\\AddOns\\Blueberry\\blue.ogg", "Master");
+    buffIsActive = true
+    PlaySoundFile("Interface\\AddOns\\Blueberry\\blue.ogg", "Master")
   else
-    buffIsAtive = false;
+    buffIsActive = false
   end
 end)
