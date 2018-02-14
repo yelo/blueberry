a3e26e9034e4a4c65af261b381c4c065d099b4b5
Jimmy Kumpulainen <jimmy@kumpulainen.se>
2018-02-14 21:43:04 +0100

    Play once per buff instance

--- blueberry.lua
+++ blueberry.lua
@@ -1,16 +1,18 @@
 -- blueberry.lua
 -- play a silly song when you go entropic
-local bluebuff = "Entropic Embrace"
-local buffIsActive = false;
-local frame = CreateFrame("FRAME", "BlueberryFrame")
-local willPlay, soundHandle = nil;
+local state = { ["Entropic Embrace"] = false }
 
-frame:RegisterUnitEvent("UNIT_AURA")
-frame:SetScript("OnEvent", function(self, event, ...)
-  if UnitBuff("player", bluebuff) and not buffIsActive then
-    buffIsActive = true
-    PlaySoundFile("Interface\\AddOns\\Blueberry\\blue.ogg", "Master")
-  else
-    buffIsActive = false
+local bf = CreateFrame("FRAME", "BlueberryFrame")
+bf:RegisterUnitEvent("UNIT_AURA", "player")
+bf:SetScript("OnEvent", function(self, event, ...)
+  for buff, active in pairs(state) do
+    if UnitBuff("player", buff) then
+      if not active then
+        PlaySoundFile("Interface\\AddOns\\Blueberry\\blue.ogg", "Master")
+        state[buff] = true
+      end
+    elseif active then
+      state[buff] = false
+    end
   end
 end)
